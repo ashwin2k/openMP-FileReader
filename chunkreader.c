eb7546418668bc7fc60953a53d34fdcaff01b73f
Karthikeyan Shanmugam <karthikeyan.shanmugam25@gmail.com>
2023-11-26 21:08:50 -0500

    cleaned up chunkreader

--- chunkreader.c
+++ chunkreader.c
@@ -1,45 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 #include <omp.h>
 #include <string.h>
 
-#define FILE_SIZE_GB 1
-#define BUFFER_SIZE 1024
-#define CHUNK_SIZE 1024
-#define NUM_THREADS 5
+#include "common.h"
+
 typedef struct{
     long start;
     long end;
     long chunk_size;
 } Range;
-
-void create_new_random_file(){
-    FILE *file;
-    char buffer[BUFFER_SIZE];
-    srand(time(NULL));
-
-    file = fopen("random_file.txt", "wb");
-
-    if (file == NULL) {
-        perror("Error opening file");
-        return;
-    }
-
-    while (ftell(file) < FILE_SIZE_GB * 1024L* 1024L* 1024L ) {
-        for (int i = 0; i < BUFFER_SIZE; i++) {
-            buffer[i] = 'A' + rand() % 26;  // Random characters (0 to 255)
-        }
-
-        fwrite(buffer, sizeof(char), BUFFER_SIZE, file);
-    }
-
-    fclose(file);
-
-    printf("File created successfully for %dGB.\n",FILE_SIZE_GB);
-
-}
 
 int read_chunk_small(long chunk_number,char* main_data,FILE* file){
     // char *buffer = (char *) malloc(CHUNK_SIZE);
@@ -49,101 +20,22 @@ int read_chunk_small(long chunk_number,char* main_data,FILE* file){
     return bytesRead;
 }
 
-long get_file_size(FILE *file){
-    long size;
+int main(int argc, char *argv[]){
+    const char* filepath = argv[1];
+    int t = atoi(argv[2]);
 
-    long currentPosition = ftell(file);
-
-    // Move to the end of the file
-    fseek(file, 0, SEEK_END);
-
-    // Get the current position, which is the size of the file
-    size = ftell(file);
-
-    // Restore the original position in the file
-    fseek(file, currentPosition, SEEK_SET);
-
-    return size;
-
-}
-long get_num_chunks(long file_size){
-    return ceil(file_size/CHUNK_SIZE);
-}
-
-int isNumberPresent(long arr[], int size, long target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i;  // Number is present
-        }
-    }
-    return -1;  // Number is not present
-}
-
-double findMax(double arr[], int size) {
-    if (size <= 0) {
-        // Handle empty array or invalid size
-        printf("Invalid array size.\n");
-        return -1;  // Returning a special value to indicate an error
-    }
-
-    double max = arr[0];  // Assume the first element is the maximum
-
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
-            max = arr[i];  // Update the maximum if a larger element is found
-        }
-    }
-
-    return max;
-}
-
-double findMin(double arr[], int size) {
-    if (size <= 0) {
-        // Handle empty array or invalid size
-        printf("Invalid array size.\n");
-        return -1;  // Returning a special value to indicate an error
-    }
-
-    double min = arr[0];  // Assume the first element is the minimum
-
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < min) {
-            min = arr[i];  // Update the minimum if a smaller element is found
-        }
-    }
-
-    return min;
-}
-
-double findAverage(double arr[], int size) {
-    if (size <= 0) {
-        // Handle empty array or invalid size
-        printf("Invalid array size.\n");
-        return -1.0;  // Returning a special value to indicate an error
-    }
-
-    double sum = 0;
-
-    for (int i = 0; i < size; i++) {
-        sum += arr[i];  // Sum up all the elements in the array
-    }
-
-    return (double)sum / size;  // Calculate the average
-}
-
-int main(){
     srand((unsigned int)10);
     // create_new_random_file();
-    FILE *file;
+    // FILE *file;
     char buffer[1024];
     size_t bytesRead;
     // Open the file
-    file = fopen("random_file.txt", "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
-    long file_size = get_file_size(file);
+    // file = fopen("random_file.txt", "r");
+    // if (file == NULL) {
+    //     perror("Error opening file");
+    //     return EXIT_FAILURE;
+    // }
+    long file_size = get_file_size(filepath);
     long num_chunks = get_num_chunks(file_size);
     
     long rand_chunks[10];
@@ -160,13 +52,13 @@ int main(){
 
     long total = 0;
     
-    FILE *filePointers[NUM_THREADS];
-    for(int i =0;i<NUM_THREADS;i++){
+    FILE *filePointers[t];
+    for(int i = 0; i < t; i++){
         filePointers[i]=fopen("random_file.txt", "r");
     }
 
     double start = omp_get_wtime();
-    #pragma omp parallel for reduction(+:total) num_threads(NUM_THREADS)
+    #pragma omp parallel for reduction(+:total) num_threads(t)
     for(long i=0;i<num_chunks;i++){
 
         long read_len = read_chunk_small(i,main_data,filePointers[omp_get_thread_num()]);
