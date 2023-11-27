6ad3c94bc8f172cc64c9d8634154707b6534d8d2
Karthikeyan Shanmugam <karthikeyan.shanmugam25@gmail.com>
2023-11-27 03:43:11 -0500

    test

--- raid.c
+++ raid.c
@@ -79,12 +79,16 @@ int main(int argc, char *argv[]) {
     #pragma omp parallel for num_threads(t) reduction(+:total)
     for(long i = 0; i < num_chunks; i++){
 
-        int threadID = omp_get_thread_num(); 
-        // int cur_chunk_size = (i == num_chunks - 1) ? file_size - i * CHUNK_SIZE : CHUNK_SIZE;       
+        int cur_chunk_size2 = (i == num_chunks - 1) ? file_size - i * CHUNK_SIZE : CHUNK_SIZE;       
         long start = i * CHUNK_SIZE;
         long end = (i == num_chunks - 1) ? file_size : (i + 1) * CHUNK_SIZE;
-        int cur_chunk_size = end - start;        
-        long local_count = readChunk(file_pointers[threadID], start, cur_chunk_size, main_data);
+        int cur_chunk_size = end - start;
+        if (cur_chunk_size2 != cur_chunk_size){
+            printf("ERROR: cur_chunk_size2 != cur_chunk_size\n");
+            printf("cur_chunk_size2: %d\n", cur_chunk_size2);
+            printf("cur_chunk_size: %d\n", cur_chunk_size);
+        }    
+        long local_count = readChunk(file_pointers[omp_get_thread_num()], start, cur_chunk_size, main_data);
 
 
 
